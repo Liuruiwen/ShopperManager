6a42eaf0ad66d264dab80304c0d68b4bfcd4a90b
Liuruiwen <1054750389@qq.com>
2021-08-24 22:47:29 +0800

    更新了个人中心布局

--- PersonalCenter/src/main/java/com/rw/personalcenter/PersonalCenterFragment.kt
+++ PersonalCenter/src/main/java/com/rw/personalcenter/PersonalCenterFragment.kt
@@ -5,6 +5,7 @@ import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.rw.basemvp.BaseFragment
 import com.rw.basemvp.presenter.DefaultPresenter
+import com.rw.personalcenter.presenter.PersonalCenterPresenter
 import com.rw.service.ServiceViewModule
 import com.rw.service.bean.LoginOutBean
 import kotlinx.android.synthetic.main.pc_fragment.*
@@ -15,27 +16,27 @@ import kotlinx.android.synthetic.main.pc_fragment.*
  * Desc:
  */
 @Route(path = "/pc/PersonalCenterFragment")
-class PersonalCenterFragment : BaseFragment<DefaultPresenter>() {
+class PersonalCenterFragment : BaseFragment<PersonalCenterPresenter>() {
     override fun getViewLayout(): Int {
        return R.layout.pc_fragment
     }
 
     override fun initView() {
-        login_state.setOnClickListener {
-            ARouter.getInstance().build("/login/LoginActivity").navigation()
-        }
-
-        login_out.setOnClickListener {
-            ServiceViewModule.get()?.loginOutService?.value= LoginOutBean()
-        }
-        ServiceViewModule.get()?.loginService?.observeForever {
-            login_state.text="用户${it.account}"
-            login_out.visibility= View.VISIBLE
-        }
-        ServiceViewModule.get()?.loginOutService?.observeForever {
-            login_state.text="未登录"
-            login_out.visibility= View.GONE
-        }
+//        login_state.setOnClickListener {
+//            ARouter.getInstance().build("/login/LoginActivity").navigation()
+//        }
+//
+//        login_out.setOnClickListener {
+//            ServiceViewModule.get()?.loginOutService?.value= LoginOutBean()
+//        }
+//        ServiceViewModule.get()?.loginService?.observeForever {
+//            login_state.text="用户${it.account}"
+//            login_out.visibility= View.VISIBLE
+//        }
+//        ServiceViewModule.get()?.loginOutService?.observeForever {
+//            login_state.text="未登录"
+//            login_out.visibility= View.GONE
+//        }
 
     }
 
@@ -43,7 +44,7 @@ class PersonalCenterFragment : BaseFragment<DefaultPresenter>() {
 
     }
 
-    override fun getPresenter(): DefaultPresenter {
-        return DefaultPresenter()
+    override fun getPresenter(): PersonalCenterPresenter {
+        return PersonalCenterPresenter()
     }
 }
