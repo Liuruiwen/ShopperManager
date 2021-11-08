932fdf2539858609593cc0e38b7f2b1d52015a61
Liuruiwen <1054750389@qq.com>
2021-11-08 23:22:59 +0800

    更新了商品品类排序处理

--- HomePage/src/main/java/com/rw/homepage/adapter/CategoryListAdapter.kt
+++ HomePage/src/main/java/com/rw/homepage/adapter/CategoryListAdapter.kt
@@ -1,6 +1,9 @@
 package com.rw.homepage.adapter
 
+import androidx.core.content.ContextCompat
 import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.module.BaseDraggableModule
+import com.chad.library.adapter.base.module.DraggableModule
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.rw.homepage.R
 import com.rw.homepage.bean.CategoryResultBean
@@ -10,10 +13,14 @@ import com.rw.homepage.bean.CategoryResultBean
  * Date:2021/10/19.
  * Desc:
  */
-class CategoryListAdapter :BaseQuickAdapter<CategoryResultBean,BaseViewHolder>(R.layout.hp_item_category_list) {
+class CategoryListAdapter :BaseQuickAdapter<CategoryResultBean,BaseViewHolder>(R.layout.hp_item_category_list),
+    DraggableModule {
     override fun convert(holder: BaseViewHolder, item: CategoryResultBean) {
         holder.setText(R.id.tv_position,"${holder.adapterPosition}")
         holder.setText(R.id.tv_category_name,item.categoryName)
         holder.setText(R.id.tv_category_desc,item.categoryDesc)
+        holder.setTextColor(R.id.tv_position,ContextCompat.getColor(context,R.color.colorWrite))
     }
+
+
 }
--- HomePage/src/main/java/com/rw/homepage/ui/activity/EditCategoryActivity.kt
+++ HomePage/src/main/java/com/rw/homepage/ui/activity/EditCategoryActivity.kt
@@ -1,13 +1,28 @@
 package com.rw.homepage.ui.activity
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.chad.library.adapter.base.listener.OnItemDragListener
+import com.chad.library.adapter.base.listener.OnItemSwipeListener
 import com.rw.basemvp.BaseActivity
 import com.rw.basemvp.widget.TitleView
+import com.rw.homepage.HttpApi
 import com.rw.homepage.R
+import com.rw.homepage.adapter.CategoryListAdapter
+import com.rw.homepage.bean.CategoryBean
 import com.rw.homepage.presenter.GoodsManagerPresenter
+import com.rw.personalcenter.until.setVisible
+import kotlinx.android.synthetic.main.hp_activity_edit_category.*
+import kotlinx.android.synthetic.main.hp_activity_goods_manager.*
 
 class EditCategoryActivity : BaseActivity<GoodsManagerPresenter>() {
 
+    private val mAdapter: CategoryListAdapter by lazy{
+        CategoryListAdapter()
+    }
 
     override fun setLayout(): Int {
        return R.layout.hp_activity_edit_category
@@ -15,9 +30,82 @@ class EditCategoryActivity : BaseActivity<GoodsManagerPresenter>() {
 
     override fun initData(savedInstanceState: Bundle?, titleView: TitleView) {
         titleView.setTitle("品类管理")
+        initView()
+        request()
     }
 
     override fun getPresenter(): GoodsManagerPresenter {
         return GoodsManagerPresenter()
     }
+
+    private fun initView(){
+        rv_category.apply {
+           layoutManager=LinearLayoutManager(this@EditCategoryActivity)
+            adapter=mAdapter
+        }
+//        mAdapter.draggableModule.isDragEnabled=true
+//        mAdapter.draggableModule.setOnItemDragListener(object : OnItemDragListener {
+//            override fun onItemDragMoving(
+//                source: RecyclerView.ViewHolder?,
+//                from: Int,
+//                target: RecyclerView.ViewHolder?,
+//                to: Int
+//            ) {
+//
+//            }
+//
+//            override fun onItemDragStart(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+//
+//            }
+//
+//            override fun onItemDragEnd(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+//                showToast("${mAdapter.getItem(pos).categoryName}")
+//            }
+//
+//        })
+        mAdapter.apply {
+            draggableModule.isDragEnabled=true
+            draggableModule.setOnItemDragListener(object : OnItemDragListener {
+                override fun onItemDragMoving(
+                    source: RecyclerView.ViewHolder?,
+                    from: Int,
+                    target: RecyclerView.ViewHolder?,
+                    to: Int
+                ) {
+
+                }
+
+                override fun onItemDragStart(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+
+                }
+
+                override fun onItemDragEnd(viewHolder: RecyclerView.ViewHolder?, pos: Int) {
+                    showToast("${mAdapter.getItem(pos).categoryName}")
+                }
+
+            })
+            setOnItemClickListener { _, _, position ->  showToast("${mAdapter.getItem(position).categoryName}")}
+        }
+        getViewModel()?.baseBean?.observe(this, Observer {
+            when (it?.requestType) {
+                HttpApi.HTTP_GET_CATEGORY_LIST -> {
+                    if (it is CategoryBean){
+                          mAdapter.setNewInstance(it.data)
+                    }
+
+
+//                    tvRight?.text=if (!data.data.isNullOrEmpty()) "管理" else "添加品类"
+                }
+                HttpApi.HTTP_ADD_CATEGORY -> {
+                    mPresenter?.reqCategory()
+                }
+                else -> showToast("系统异常")
+            }
+        })
+    }
+
+    private fun request(){
+           mPresenter?.reqCategory()
+    }
+
 }
